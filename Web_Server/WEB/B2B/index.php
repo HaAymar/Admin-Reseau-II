f075516e048177a9c1252a2869b8b675f6186b52
HaAymar <he201905@students.ephec.be>
2022-06-12 23:09:25 +0200

    [UPDATE]Update of file 'web_config'

---
+++ Web_Server/WEB/B2B/index.php
@@ -0,0 +1,32 @@
+<?php
+$mysqli = new mysqli("176.96.231.163:5000",'root','admin','woodytoys_db');
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Refused to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+?>
+
+<html>
+
+<head>
+</head>
+
+<body>
+    <h1>Nous vous souhaitons le bienvenue sur notre site " WoodyToys l1-4 !</h1>
+
+    <?php
+        $query = "SELECT * FROM toys;";
+        mysqli_query($mysqli, $query) or die('Error querying database.');
+        $result = mysqli_query($mysqli, $query);
+
+        while ($row = mysqli_fetch_array($result)) {
+            echo $row['id_toys'] . ': ' . $row['toyName'] . ' ' . $row['toyPrice'] . ' <br />';
+        }
+    ?>
+
+</body>
+
+</html>
